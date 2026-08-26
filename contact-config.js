513fa6ca3afa68862984cff4de74c0fc3e63ca6d
Raushan Kumar <raushanmansagar12345@gmail.com>
2026-08-26 20:20:53 +0530

    Add contact API configuration

---
+++ contact-config.js
@@ -0,0 +1,2 @@
+window.CONTACT_API_URL = 'https://tvquwukbcruryrkhqkkc.supabase.co/functions/v1/contact-enquiry';
+window.CERTIFICATE_VERIFY_API_URL = 'https://tvquwukbcruryrkhqkkc.supabase.co/functions/v1/certificate-verify';
